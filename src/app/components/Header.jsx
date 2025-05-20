7a43c45e068927631156c3f0d0834dbc5446286c
jukalbf <lgbr212@gmail.com>
2025-05-20 14:59:05 -0300

    header finalizada

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,15 +1,16 @@
-import UserIcon from "../../assets/icons/user.svg";
+import { UserRound } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="w-full p-5 flex justify-evenly">
+    <header className="w-full p-3 flex justify-between items-center">
       <img
         src="https://di2win-site.b-cdn.net/wp-content/uploads/2024/05/cropped-logo-completa-preto.png"
         alt="di2win-logo"
         className="w-40"
       />
-      <div className="w-full">
-        <img src={UserIcon} alt="user-icon" />
+      <div className="flex justify-center items-center bg-pink-500 p-2 rounded-sm gap-2 pr-4 pl-4 cursor-pointer active:translate-y-0.5 ease-in-out duration-100">
+        <UserRound color="white" />
+        <h3 className="text-white select-none">Equipe 1</h3>
       </div>
     </header>
   );
