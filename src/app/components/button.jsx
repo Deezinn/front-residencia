0924e2c01a59b4037ce87fb0724939a76c58ed6b
Deezinn <developer.andrelz@gmail.com>
2025-05-19 22:35:41 -0300

    fix: style button

--- src/app/components/button.jsx
+++ src/app/components/button.jsx
@@ -5,14 +5,14 @@ export default function Button() {
     <div className="flex flex-col items-center mt-20 space-y-1">
       <button
         style={{ backgroundColor: '#c10075' }}
-        className="shadow-md rounded-sm w-52 h-14 flex items-center mt-5 text-lg space-x-2 focus:outline-1 text-white"
+        className="shadow-md active:shadow-inner active:translate-y-2 duration-200 rounded-sm w-52 h-14 flex items-center justify-items-start"
       >
         <img
           src="https://cdn-icons-png.flaticon.com/128/7781/7781587.png"
           alt="dashboard-icon"
           className="w-5 h-auto opacity-90 ml-4"
         />
-        <span className="ml-2 text-black">Dashboard</span>
+        <span className="ml-5 text-black font-sans">Dashboard</span>
       </button>
     </div>
   );
