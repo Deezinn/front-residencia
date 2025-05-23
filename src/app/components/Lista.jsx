25c29bb82f55beaf773421a7e7668c31390c6a40
jukalbf <lgbr212@gmail.com>
2025-05-23 01:55:48 -0300

    ajustes-border

--- src/app/components/Lista.jsx
+++ src/app/components/Lista.jsx
@@ -15,29 +15,56 @@ const erros = [
     modelo: "extrai",
     cliente: "Rodrigo",
   },
+  {
+    id: 5,
+    dataHora: "10-03-2025/15:24pm",
+    modelo: "extrai",
+    cliente: "Carol",
+  },
+  {
+    id: 6,
+    dataHora: "10-03-2025/15:24pm",
+    modelo: "extrai",
+    cliente: "Laura",
+  },
+  {
+    id: 7,
+    dataHora: "10-03-2025/15:24pm",
+    modelo: "extrai",
+    cliente: "Marcelo",
+  },
 ];
 
 export default function Lista() {
   const listErrors = erros.map((erro, index) => (
-    <tr key={erro.id || index} className="odd:bg-zinc-100 even:bg-zinc-300 border-b-2 border-black">
+    <tr
+      key={erro.id || index}
+      className="odd:bg-zinc-100 even:bg-zinc-300 border-b-2 border-black"
+    >
       <td className="p-1 text-center text-zinc-800 select-none">{erro.id}</td>
-      <td className="p-1 text-center text-zinc-800 select-none">{erro.dataHora}</td>
-      <td className="p-1 text-center text-zinc-800 select-none">{erro.modelo}</td>
-      <td className="p-1 text-center text-zinc-800 select-none">{erro.cliente}</td>
+      <td className="p-1 text-center text-zinc-800 select-none">
+        {erro.dataHora}
+      </td>
+      <td className="p-1 text-center text-zinc-800 select-none">
+        {erro.modelo}
+      </td>
+      <td className="p-1 text-center text-zinc-800 select-none">
+        {erro.cliente}
+      </td>
     </tr>
   ));
 
   return (
-      <table className="w-3xl border-3 border-black border-separate border-spacing-0 rounded-md overflow-hidden">
-        <thead className="bg-pink-fraco">
-          <tr className="border-b-2 border-black">
-            <th className="p-1 text-gray-800">ID</th>
-            <th className="p-1 text-gray-800">Data e Hora</th>
-            <th className="p-1 text-gray-800">Modelo</th>
-            <th className="p-1 text-gray-800">Cliente</th>
-          </tr>
-        </thead>
-        <tbody>{listErrors}</tbody>
-      </table>
+    <table className="w-3xl border-2 border-black border-separate border-spacing-0 rounded-md overflow-hidden">
+      <thead className="bg-pink-fraco">
+        <tr className="border-b-2 border-black">
+          <th className="p-1 text-gray-800">ID</th>
+          <th className="p-1 text-gray-800">Data e Hora</th>
+          <th className="p-1 text-gray-800">Modelo</th>
+          <th className="p-1 text-gray-800">Cliente</th>
+        </tr>
+      </thead>
+      <tbody>{listErrors}</tbody>
+    </table>
   );
 }
