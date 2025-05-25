94222049ebb841e00808772fc27d82456544ffc0
LucasEmmanoel06 <lucas.albuquerque2k@gmail.com>
2025-05-25 13:43:15 -0300

    colunas da lista atualizadas

--- src/app/components/Lista.jsx
+++ src/app/components/Lista.jsx
@@ -1,37 +1,61 @@
 "use client";
 
 const erros = [
-  { id: 1, dataHora: "10-03-2025/20:53pm", modelo: "extrai", cliente: "Mario" },
-  { id: 2, dataHora: "10-03-2025/08:32am", modelo: "extrai", cliente: "Clara" },
+  {
+    id: 1,
+    codigo: 500,
+    guid: "334-22052025-18257-83686fef-b1e9-44ac-b1e6-6dafe5cbca51",
+    dataHora: "14/04/2025 20:23:30",
+    modelo: "ExtrAI Dados",
+    mensagem: "Bad Request",
+  },
+  {
+    id: 2,
+    codigo: 400,
+    guid: "335-22052025-18257-83686fef-b1e9-44ac-b1e6-6dafe5cbca52",
+    dataHora: "14/04/2025 20:32:30",
+    modelo: "ExtrAI Dados",
+    mensagem: "Bad Request",
+  },
   {
     id: 3,
-    dataHora: "10-03-2025/10:11am",
-    modelo: "extrai",
-    cliente: "Fernanda",
+    codigo: 404,
+    guid: "336-22052025-18257-83686fef-b1e9-44ac-b1e6-6dafe5cbca53",
+    dataHora: "14/04/2025 20:42:30",
+    modelo: "ExtrAI Dados",
+    mensagem: "Not Found",
   },
   {
     id: 4,
-    dataHora: "10-03-2025/15:24pm",
-    modelo: "extrai",
-    cliente: "Rodrigo",
+    codigo: 500,
+    guid: "337-22052025-18257-83686fef-b1e9-44ac-b1e6-6dafe5cbca54",
+    dataHora: "14/04/2025 20:52:30",
+    modelo: "ExtrAI Dados",
+    mensagem: "Internal Server Error",
   },
   {
     id: 5,
-    dataHora: "10-03-2025/15:24pm",
-    modelo: "extrai",
-    cliente: "Carol",
+    codigo: 400,
+    guid: "338-22052025-18257-83686fef-b1e9-44ac-b1e6-6dafe5cbca55",
+    dataHora: "14/04/2025 21:02:30",
+    modelo: "ExtrAI Dados",
+    mensagem: "Bad Request",
   },
   {
     id: 6,
-    dataHora: "10-03-2025/15:24pm",
-    modelo: "extrai",
-    cliente: "Laura",
+    codigo: 404,
+    guid: "339-22052025-18257-83686fef-b1e9-44ac-b1e6-6dafe5cbca56",
+    dataHora: "14/04/2025 21:12:30",
+    modelo: "ExtrAI Dados",
+    mensagem: "Not Found",
   },
   {
     id: 7,
-    dataHora: "10-03-2025/15:24pm",
-    modelo: "extrai",
-    cliente: "Marcelo",
+    codigo: 500,
+    guid: "340-22052025-18257-83686fef-b1e9-44ac-b1e6-6dafe5cbca57",
+    dataHora: "14/04/2025 21:22:30",
+    modelo: "ExtrAI Dados",
+    mensagem: "Internal Server Error",
   },
 ];
 
@@ -43,13 +67,17 @@ export default function Lista() {
     >
       <td className="p-1 text-center text-zinc-800 select-none">{erro.id}</td>
       <td className="p-1 text-center text-zinc-800 select-none">
+        {erro.codigo}
+      </td>
+      <td className="p-1 text-center text-zinc-800 select-none">{erro.guid}</td>
+      <td className="p-1 text-center text-zinc-800 select-none">
         {erro.dataHora}
       </td>
       <td className="p-1 text-center text-zinc-800 select-none">
         {erro.modelo}
       </td>
       <td className="p-1 text-center text-zinc-800 select-none">
-        {erro.cliente}
+        {erro.mensagem}
       </td>
     </tr>
   ));
@@ -58,10 +86,12 @@ export default function Lista() {
     <table className="w-3xl border-2 border-black border-separate border-spacing-0 rounded-md overflow-hidden">
       <thead className="bg-pink-fraco dark:bg-pink-800">
         <tr className="border-b-2 border-black">
-          <th className="p-1 text-gray-800">ID</th>
-          <th className="p-1 text-gray-800">Data e Hora</th>
-          <th className="p-1 text-gray-800">Modelo</th>
-          <th className="p-1 text-gray-800">Cliente</th>
+          <th className="px-3 py-2 text-gray-800">ID</th>
+          <th className="px-3 py-2 text-gray-800">Código</th>
+          <th className="px-3 py-2 text-gray-800">guid</th>
+          <th className="px-3 py-2 text-gray-800">Data e Hora</th>
+          <th className="px-3 py-2 text-gray-800">Modelo</th>
+          <th className="px-3 py-2 text-gray-800">Mensagem</th>
         </tr>
       </thead>
       <tbody>{listErrors}</tbody>
