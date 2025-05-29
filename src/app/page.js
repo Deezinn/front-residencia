1c5b3f7f607c6e2ac266fc0cde68271b35171d69
LucasEmmanoel06 <lucas.albuquerque2k@gmail.com>
2025-05-29 20:18:34 -0300

    feat: logo err2log e texto adicionados na pagina inicial

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 export default function Main() {
   return (
-    <div className="">
-      
+    <div className="p-20">
+      <img src="../assets/img/err2log.png" alt="Err2Log Logo" className="w-70"/>
+      <h1 className="pl-2 text-4xl font-bold text-black">
+        Registros de erro da API ExtrAI Dados
+      </h1>
     </div>
   );
 }
